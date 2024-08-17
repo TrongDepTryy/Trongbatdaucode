d3be4ce4758b190a580c76877b6b9a859f70cc4b
TrongDepTryy <23719901.trong@student.iuh.edu.vn>
2024-08-18 00:25:10 +0700

    Update chia dư nguyên.py
    V1.0.0.1
    thêm các mô tả

--- chia dư nguyên.py
+++ chia dư nguyên.py
@@ -8,7 +8,9 @@ Created on Wed Aug 14 21:29:14 2024
 
 a = float(input( "Hãy nhập số thứ 1 vào nhé : "))
 b = float(input("Hãy nhập số thứ 2 vào nhé :"))
+#sub này là biến dùng để lấy phần nguyên
 sub=a/b
+#d là biến dùng để lấy phần dư
 d= a%b 
 print('Kết quả là : ')
 print("Phần nguyên là :" ,[sub])
