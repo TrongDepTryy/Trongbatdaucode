9984d17ed93f1d439cc87da984a67350ca82c90f
TrongDepTryy <23719901.trong@student.iuh.edu.vn>
2024-08-25 14:53:16 +0700

    Add files via upload

---
+++ BT_6/format_pt2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 14:50:33 2024
+
+@author: trongg
+"""
+
+a = float(input( "Hãy nhập số a vào nhé : "))
+b = float(input("Hãy nhập số b  vào nhé :"))
+c = float(input("Hãy nhập số c  vào nhé :"))
+print(f" {a}X^2+{b}X+{c}=0")
